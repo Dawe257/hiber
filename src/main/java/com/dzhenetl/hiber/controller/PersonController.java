ddf015d6daf45bc69688d58f97d2a927c6a9f746
dzhenetl <zhenetldm@it.mos.ru>
2022-12-07 09:39:38 +0300

    JpaRepository

--- src/main/java/com/dzhenetl/hiber/controller/PersonController.java
+++ src/main/java/com/dzhenetl/hiber/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController("/persons")
@@ -26,7 +27,9 @@ public class PersonController {
 
     @GetMapping("/by-age")
     public List<Person> getPersonsByCity(@RequestParam Integer age) {
-        return repository.findByAgeLessThan(Sort.by("age"), age);
+        List<Person> persons = repository.findByAgeLessThan(Sort.by("age"), age);
+        persons.sort(Comparator.comparingInt(Person::getAge));
+        return persons;
     }
 
     @GetMapping("/by-name-and-surname")
--- src/main/java/com/dzhenetl/hiber/repository/PersonRepository.java
+++ src/main/java/com/dzhenetl/hiber/repository/PersonRepository.java
@@ -2,14 +2,14 @@ package com.dzhenetl.hiber.repository;
 
 import com.dzhenetl.hiber.entity.Person;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findAllByCityOfLiving(String city);
 
